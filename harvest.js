db330f07f6b57d1b0ac47d5f96b9a5c040660a87
RichDavis1 <63970045+RichDavis1@users.noreply.github.com>
2021-07-10 14:56:17 -0500

    Edit harvest.js
    Initial commit.

---
+++ harvest.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name         Target captcha harvester
+// @namespace    richdavis
+// @version      1.0.0
+// @description  Harvest many target sessions
+// @author       living_tribunal / Rich Davis
+// @match        https://login.target.com/gsp/static/v1/login/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+"use strict";
+
+// Auto click on login button
+const AUTO_CLICK = true;
+
+(async function () {
+  document.addEventListener("DOMContentLoaded", async function () {
+      await waitForLoginButton();
+
+      var loginButton = document.getElementById("login");
+
+      if (typeof(loginButton) == 'undefined' || loginButton == null){
+          alert("login button not present");
+      }
+
+      var count = 0;
+
+      while(count < 500) {
+          await sleep(25)
+          loginButton.click();
+      }
+
+      alert("1000 cookies have been generated");
+  });
+})();
+
+// Wait for login button to load
+const waitForLoginButton = async () => {
+    loginButton = null;
+    while(loginButton == null){
+        await sleep(500)
+
+        var loginButton = document.getElementById("login");
+    }
+}
+
+// Sleep for specified delay
+const sleep = (duration = 1000) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+};
